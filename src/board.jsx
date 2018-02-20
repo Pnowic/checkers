3e870c76a4a5b13854aacfb30538087ecb9a2ec1
Piotr Nowicki <nowickipiotrus@gmail.com>
2018-02-20 19:41:55 +0100

    add config files,html file and main.jsx file add game board

---
+++ src/board.jsx
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    let drawBoard = () => {
+
+        var canvas = document.getElementById('canvas');
+        if (canvas.getContext) {
+            var ctx = canvas.getContext('2d');
+
+
+            for (let i=0; i<8; i++){ //rows
+
+                for (let j=0; j<8; j++){ //columns
+
+                    if (i%2 === j%2){ //if modulo result is the same draw black square, else draw white
+                        ctx.fillStyle = '#000000';
+                        ctx.fillRect(100*i, 100*j, 100, 100); //(position in x(row), position in y(column), width, height)
+                    } else {
+                        ctx.fillStyle = '#ffffff';
+                        ctx.fillRect(100*i, 100*j, 100, 100);
+                    }
+
+                }
+            }
+
+        }
+    };
+
+    drawBoard ();
+});
+
+
+module.export = drawBoard;
---
+++ src/main.jsx
@@ -0,0 +1,3 @@
+let drawBoard = require("./board.jsx");
+
+
